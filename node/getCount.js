bb3e1db853aeb574f2d5f8a45d8fb11338d0fc7c
levsthings <bearnode@gmail.com>
2017-10-19 13:02:11 +0300

    add count example

---
+++ node/getCount.js
@@ -0,0 +1,22 @@
+'use strict'
+const AWS = require('aws-sdk')
+const config = require('../config/config')
+
+AWS.config.update({
+    region: 'us-east-2',
+    accessKeyId: config.id,
+    secretAccessKey: config.key
+})
+
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+const params = {
+    TableName: 'ExampleTable',
+    ProjectionExpression: 'id'
+}
+
+docClient.scan(params, (error, data) => {
+    error
+        ? console.error('Unable to get item. Error:', JSON.stringify(error, null, 4))
+        : console.log('Value: ', JSON.stringify(data, null, 4))
+})
